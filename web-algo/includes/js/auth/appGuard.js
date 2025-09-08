ba10332bae225cb0704205571a7530140cb7478b
GabrielMosconegi <gabriel.gallina@taking.com.br>
2025-09-08 20:48:08 -0300

    feat: acessiblity fontsize & theme

--- web-algo/includes/js/auth/appGuard.js
+++ web-algo/includes/js/auth/appGuard.js
@@ -1,16 +1,16 @@
-import { isAuthenticated, logout } from './auth.js';
+import { isAuthenticated, logout } from './auth.js'
 
 if (!isAuthenticated()) {
-    location.replace('login.html');
+  // location.replace('login.html');
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('logoutBtn');
-    if (btn) {
-        btn.addEventListener('click', async (e) => {
-            e.preventDefault();
-            await logout();
-            location.replace('login.html');
-        });
-    }
-});
+  const btn = document.getElementById('logoutBtn')
+  if (btn) {
+    btn.addEventListener('click', async (e) => {
+      e.preventDefault()
+      await logout()
+      location.replace('login.html')
+    })
+  }
+})
